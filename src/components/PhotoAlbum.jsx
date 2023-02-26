2e674d1780bf862ba889ffdaa9ed993db205e995
efra-tech <eahsan@uw.edu>
2023-02-25 20:30:14 -0800

    Setting up navbar

--- src/components/PhotoAlbum.jsx
+++ src/components/PhotoAlbum.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import React from 'react';
 
-const PhotoAlbum = () => {
+const PhotoAlbum = (props) => {
     return (
       <div className="photoalbum">
         <h2>PhotoAlbum page</h2>
--- src/index.js
+++ src/index.js
@@ -3,4 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
