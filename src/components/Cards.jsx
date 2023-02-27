d8838c38f6eb8eeb9f50129334dc39f0a13c12f2
Kush Bhatia <kushagra1198@gmail.com>
2023-02-26 20:22:17 -0800

    added cards to photo-album

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -3,17 +3,15 @@ export function Card({ imageUrl, title, linkUrl }) {
 
     const handleClick = () => {
         window.location.href = '/home/album-name';
-      };
-    
+    };
 
-  return (
-    <a href={linkUrl} onClick={handleClick}>
-      <div className="card" onClick={handleClick}>
-        <img src={imageUrl} className="card-img" alt="Card image" />
-        <div className="card-img-overlay d-flex align-items-center">
-          <p className="card-title text-center mx-auto">{title}</p>
+
+    return (
+        <div className="card" onClick={handleClick}>
+            <img src={imageUrl} className="card-img" alt="Card image" />
+            <div className="card-img-overlay d-flex align-items-center">
+                <p className="card-title text-center mx-auto">{title}</p>
+            </div>
         </div>
-      </div>
-    </a>
-  );
+    );
 }
--- src/components/PhotoAlbum.jsx
+++ src/components/PhotoAlbum.jsx
@@ -5,21 +5,26 @@ import PhotoGallery from './PhotoGallery';
 export default function PhotoAlbum(props) {
 
   return (
-    <div>
-
-      <div className='container'>
-        <div className='row d-flex justify-content-md-around'>
-          <Card
-            imageUrl="/images/all-images-cover.jpg"
-            title="All Images"
-            linkUrl="album-name.html"
-          />
-          {/* <CardThumbnail title='Breakfast at Lakeside' url='./PhotoGallery' type='album' />
+    <main class='container-main'>
+        <Card
+          imageUrl="/images/all-images-cover.jpg"
+          title="All Images"
+          linkUrl="album-name.html"
+        />
+        <Card
+          imageUrl="/images/kids-album-cover1.jpg"
+          title="Kids Album"
+          linkUrl="album-name.html"
+        />
+        <Card
+          imageUrl="/images/vacation-album-cover.jpg"
+          title="Vacation Album"
+          linkUrl="album-name.html"
+        />
+        
+        {/* <CardThumbnail title='Breakfast at Lakeside' url='./PhotoGallery' type='album' />
           <CardThumbnail title='Christmas in Cabo' url='./PhotoGallery' type='album' />
           <CardThumbnail title='Christmas in Cabo' url='./PhotoGallery' type='album' /> */}
-        </div>
-      </div>
-
-    </div>
+    </main>
   );
 }
