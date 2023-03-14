500045a205af7091dd5e92d6304b3f5bb8a0de4d
carl-searle <carl1298@outlook.com>
2023-03-13 20:45:03 -0700

    part of user auth and register page

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
-import { BrowserRouter as Routes, Route } from 'react-router-dom';
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom';
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+
+const auth = getAuth();
 
 const Register = () => {
+  const [inputEmail, setInputEmail] = useState('');
+  const [inputPassword, setInputPassword] = useState('');
+
+  const changeEmail = (event) => {
+    setInputEmail(event.target.value);
+  }
+
+  const changePassword = (event) => {
+    setInputPassword(event.target.value);
+  }
+
   return (
     <>
       <div className="register">
-        <h2>Add Register page</h2>
+        <h1>Register page</h1>
+        <label>
+          Email:
+          <input type='inputEmail' value={inputEmail} onChange={changeEmail} />
+        </label>
+        <label>
+          Password:
+          <input type='inputPassword' value={inputPassword} onChange={changePassword} />
+        </label>
+        <button onClick={changeEmail}>Sign Up</button>
+        <Link to='/login'>Log in here if you already have an account.</Link>
       </div>
     </>
   );
--- src/index.js
+++ src/index.js
@@ -4,11 +4,21 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import './index.css';
 import App from './App';
+import { Link } from 'react-router-dom';
+import Register from './components/Register';
 
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
+import {
+  getAuth, 
+  connectAuthEmulator, 
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut
+} from 'firebase/auth';
 
 const firebaseConfig = {
   apiKey: "AIzaSyDWtOB1MPwlz3f801ANOFN-YHTuuOV_zCE",
@@ -19,12 +29,47 @@ const firebaseConfig = {
   appId: "1:901039374069:web:e03589b1b3fb6817d04fbc"
 };
 
+const auth = getAuth(App);
+connectAuthEmulator(auth, 'http://localhost:3000');
+
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const storage = getStorage(app);
 
+const checkAuth = async () => {
+  //I don't know how to pass the password and email value over, just wrote this here for now.
+  const inputEmail = txtEmail.value;
+  const inputPassword = txtPassword.value;
+  const credentials = await signInWithEmailAndPassword(auth, inputEmail, inputPassword);
+  console.log(credentials.user);
+}
+
+loginButton.addEventListener('click', checkAuth);
+
+const makeAccount = async () => {
+  const inputEmail = document.getElementById('txtEmail').value;
+  const inputPassword = document.getElementById('txtPassword').value;
+  const credentials = await createUserWithEmailAndPassword(auth, inputEmail, inputPassword);
+  console.log(credentials.user);
+}
+
+signUpButton.addEventListener('click', makeAccount);
+
+const authState = async () => {
+  onAuthStateChanged(auth, user => {
+    if(user) {
+      <Link to={'/components/Home'}></Link>
+    }
+  });
+}
+
+authState();
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <App />
+  <>
+    <App />
+    <Register />
+  </>
 );
